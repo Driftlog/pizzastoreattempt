dae488696be714849bfbaaeaaaa267dd9c04384b
driftlog <driftlogged@gmail.com>
2023-06-07 16:16:47 +0800

    first-commit

---
+++ src/main/java/sg/edu/nus/iss/pizzastoreattempt/PizzastoreattemptApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.pizzastoreattempt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzastoreattemptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzastoreattemptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/pizzastoreattempt/PizzastoreattemptApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.pizzastoreattempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzastoreattemptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
